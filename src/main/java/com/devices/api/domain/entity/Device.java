63402818ee2779fe0a613d05ba4de016d6b22ce8
jose durigon <jose.durigon@outlook.com>
2025-11-02 17:20:39 -0300

    creating the validations and use cases for devices. Cannot be deleted while in use, name and brand cannot be changed while in use also. There's no framework in here. Just pure java code since we're talking about our logic and the core may not depend on things that are too much low level like a framework

--- src/main/java/com/devices/api/domain/entity/Device.java
+++ src/main/java/com/devices/api/domain/entity/Device.java
@@ -3,6 +3,7 @@ package com.devices.api.domain.entity;
 import com.devices.api.domain.enun.DeviceState;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Device {
     private long id;
@@ -37,4 +38,79 @@ public class Device {
 
     }
 
+    private void update(String newName, String newBrand, DeviceState newState){
+        if (this.state == DeviceState.IN_USE){
+
+            if (!Objects.equals(newName, this.name) || !Objects.equals(newBrand, this.brand)){
+                throw new IllegalStateException("You shall not update a device that's already in use.");
+            }
+        }
+
+
+        if (this.name != null && !newName.isBlank()){
+            this.name = newName;
+        }
+
+        if (this.brand != null && !newBrand.isBlank()){
+            this.brand = newBrand;
+        }
+
+        if (newState != null)
+            this.state = newState;
+    }
+
+
+    public void makeSureDeviceCanBeDeleted(){
+        if (this.state == DeviceState.IN_USE)
+            throw new IllegalStateException("Device will not be deleted because it's currently in use!");
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public DeviceState getState() {
+        return state;
+    }
+
+    public void setState(DeviceState state) {
+        this.state = state;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Device{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", state=" + state +
+                ", creationTime=" + creationTime +
+                '}';
+    }
 }
