64adc990558c508ec321a50dec1d789ef7fe3ed4
jose durigon <jose.durigon@outlook.com>
2025-11-02 18:17:47 -0300

    Created the another methods in the gateway. It has another presistence method. The save contract will come in handy because this way we'll have only one contract for both creating a new device and updating it.

--- src/main/java/com/devices/api/gateway/DeviceRepositoryGateway.java
+++ src/main/java/com/devices/api/gateway/DeviceRepositoryGateway.java
@@ -3,9 +3,14 @@ package com.devices.api.gateway;
 import com.devices.api.domain.entity.Device;
 import com.devices.api.domain.enun.DeviceState;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 public interface DeviceRepositoryGateway {
     List<Device> findAll();
-    Device createDevice(String name, String brand, DeviceState state);
+    Device save(Device device); //save can be used both for update and create
+    Optional<Device> findById(Long id);
+    void delete (Long id);
+
 }
