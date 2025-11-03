3849e9023a39a03a2b04bc1136405af9df72f259
jose durigon <jose.durigon@outlook.com>
2025-11-02 22:15:13 -0300

    added device entity class which will be the entity used by Spring Data for mapping the table within our databse. using Lombock and annotations to lock the max characters and setting field that are not updatable such as creation date

--- src/main/java/com/devices/api/infra/persistence/jpa/DeviceEntity.java
+++ src/main/java/com/devices/api/infra/persistence/jpa/DeviceEntity.java
@@ -1,17 +1,38 @@
 package com.devices.api.infra.persistence.jpa;
 
 
-import jakarta.persistence.Entity;
+import com.devices.api.domain.enun.DeviceState;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
+@Table(name = "devices")
 @Data
 @RequiredArgsConstructor
 @NoArgsConstructor
 public class DeviceEntity {
 
-    //TODO: implementar a entidade de banco propriamente dita. Criar ou nao criar tabela que seria a enum?
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "brand", nullable = false, length = 40)
+    private String brand;
+
+    @Column(name = "state", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DeviceState state;
+
+    @Column(name = "creation_time", nullable = false, updatable = false)
+    private LocalDateTime creationTime;
+
 
 }
